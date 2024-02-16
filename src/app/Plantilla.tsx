89743b9cb475e7a37f57ef6d3deee0176406c2d7
Francisco Alvarado <franciscolea@hotmail.com>
2024-02-16 15:49:54 -0600

    update vercel

--- src/app/Plantilla.tsx
+++ src/app/Plantilla.tsx
@@ -29,70 +29,70 @@ export const Plantilla = () => {
       <div className="content-box-container">
         <div className="content-box-buttons">
           <button
-            className={`${info === 'descripcion' ? 'clicked' : null}`}
+            className={`${info === "descripcion" ? "clicked" : null}`}
             onClick={(event) => {
               cambiarInfo(event.currentTarget.value);
             }}
-            value={'descripcion'}
+            value={"descripcion"}
           >
             DESCRIPCION
           </button>
           <button
-            className={`${info === 'incluye' ? 'clicked' : null}`}
+            className={`${info === "incluye" ? "clicked" : null}`}
             onClick={(event) => {
               cambiarInfo(event.currentTarget.value);
             }}
-            value={'incluye'}
+            value={"incluye"}
           >
             INCLUYE
           </button>
           <button
-            className={`${info === 'ficha' ? 'clicked' : null}`}
+            className={`${info === "ficha" ? "clicked" : null}`}
             onClick={(event) => {
               cambiarInfo(event.currentTarget.value);
             }}
-            value={'ficha'}
+            value={"ficha"}
           >
             FICHA TECNICA
           </button>
           <button
-            className={`${info === 'servicio' ? 'clicked' : null}`}
+            className={`${info === "servicio" ? "clicked" : null}`}
             onClick={(event) => {
               cambiarInfo(event.currentTarget.value);
             }}
-            value={'servicio'}
+            value={"servicio"}
           >
             SERVICIO TECNICO
           </button>
           <button
-            className={`${info === 'garantia' ? 'clicked' : null}`}
+            className={`${info === "garantia" ? "clicked" : null}`}
             onClick={(event) => {
               cambiarInfo(event.currentTarget.value);
             }}
-            value={'garantia'}
+            value={"garantia"}
           >
             GARANTIA
           </button>
         </div>
         <div className="content-box-message">
-          <div style={{ display: 'flex' }}>
+          <div style={{ display: "flex" }}>
             {data.map((product: any) => (
               <div key={product.sku} className="product-info">
-                {info === 'descripcion' && (
-                  <p className="appear" style={{ margin: '0px' }}>
+                {info === "descripcion" && (
+                  <p className="appear" style={{ margin: "0px" }}>
                     {product.descripcion}
                   </p>
                 )}
-                {info === 'incluye' && (
-                  <div className="appear" style={{ margin: '0px' }}>
+                {info === "incluye" && (
+                  <div className="appear" style={{ margin: "0px" }}>
                     <ul style={{}}>
                       {product.incluye
-                        .split('\n')
+                        .split("\n")
                         .map((item: any, index: any) => (
                           <li
                             style={{
-                              paddingLeft: '10px',
-                              textAlign: 'left',
+                              paddingLeft: "10px",
+                              textAlign: "left",
                             }}
                             key={index}
                           >
@@ -102,26 +102,26 @@ export const Plantilla = () => {
                     </ul>
                   </div>
                 )}
-                {info === 'servicio' && (
-                  <p className="appear" style={{ margin: '0px' }}>
+                {info === "servicio" && (
+                  <p className="appear" style={{ margin: "0px" }}>
                     {product.serviciotecnico}
                   </p>
                 )}
-                {info === 'garantia' && (
-                  <p className="appear" style={{ margin: '0px' }}>
+                {info === "garantia" && (
+                  <p className="appear" style={{ margin: "0px" }}>
                     {product.garantia}
                   </p>
                 )}
-                {info === 'ficha' && (
-                  <div className="appear" style={{ margin: '0px' }}>
+                {info === "ficha" && (
+                  <div className="appear" style={{ margin: "0px" }}>
                     <ul style={{}}>
                       {product.fichatecnica
-                        .split('\n')
+                        .split("\n")
                         .map((item: any, index: any) => (
                           <li
                             style={{
-                              paddingLeft: '10px',
-                              textAlign: 'left',
+                              paddingLeft: "10px",
+                              textAlign: "left",
                             }}
                             key={index}
                           >
@@ -137,11 +137,11 @@ export const Plantilla = () => {
         </div>
       </div>
       <h2 className="slider-header">¿QUE MATERIALES PUEDO USAR?</h2>
-      <div className="slider-container" style={{ height: '400px' }}>
+      <div className="slider-container" style={{ height: "400px" }}>
         <div
           id="id"
           className="slider-wrapper animation"
-          style={{ width: '-1673px' }}
+          style={{ width: "-1673px" }}
         >
           {data.map((product: any) =>
             product.materiales.map((material: any) => (
@@ -156,46 +156,50 @@ export const Plantilla = () => {
         </div>
       </div>
       <h2 className="slider-header">PROYECTOS DE CLIENTES</h2>
-      <div className="slider-container" style={{ height: '400px' }}>
+      <div className="slider-container" style={{ height: "400px" }}>
         <div
           className="slider-wrapper animation"
-          style={{ marginTop: '40px', width: '-2376px' }}
+          style={{ marginTop: "40px", width: "-2376px" }}
         >
           {data.map((product: any) =>
-            product.imagenesusuarios.map((material: any) => (
-              <div className="slider-item" key={material.id}>
-                <img alt="img4" src={material.urlimage} />
-              </div>
-            ))
+            product.imagenesusuarios
+              .split("\n")
+              .map((item: any, index: any) => (
+                <div className="slider-item" key={index + item}>
+                  <img alt="img4" src={item} />
+                </div>
+              ))
           )}
         </div>
       </div>
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: "relative" }}>
         <a
           href="https://www.sagaon.tech/collections/vectores-corte-y-grabado-cnc-laser-y-router-gratis"
           target="_top"
           style={{
-            position: 'absolute',
-            right: '0px',
-            bottom: '0px',
-            width: '100%',
-            zIndex: '1',
+            position: "absolute",
+            right: "0px",
+            bottom: "0px",
+            width: "100%",
+            zIndex: "1",
           }}
         >
           <button className="simple-button">DESCARGA TUS DISEÑOS</button>
         </a>
         <h2 className="slider-header">PROYECTOS SAGAON</h2>
-        <div className="slider-container" style={{ height: '400px' }}>
+        <div className="slider-container" style={{ height: "400px" }}>
           <div
             className="slider-wrapper animation"
-            style={{ width: '-836.5px' }}
+            style={{ width: "-836.5px" }}
           >
             {data.map((product: any) =>
-              product.proyectossagaon.map((material: any) => (
-                <div className="slider-item" key={material.id}>
-                  <img alt="img4" src={material.urlimage} />
-                </div>
-              ))
+              product.proyectossagaon
+                .split("\n")
+                .map((item: any, index: any) => (
+                  <div className="slider-item" key={index + item}>
+                    <img alt="img4" src={item} />
+                  </div>
+                ))
             )}
           </div>
         </div>
@@ -213,7 +217,7 @@ export const Plantilla = () => {
           ></iframe>
         </div>
       </div>
-      <div style={{ marginTop: '150px', position: 'relative' }}>
+      <div style={{ marginTop: "150px", position: "relative" }}>
         <img
           className="footer-img"
           alt="img-CNC3020"
@@ -226,11 +230,11 @@ export const Plantilla = () => {
           download=""
           href="/static/media/CNC3020.f93f8510d887c32e7041.pdf"
           style={{
-            position: 'absolute',
-            top: '60%',
-            zIndex: '1',
-            left: '0px',
-            width: '100%',
+            position: "absolute",
+            top: "60%",
+            zIndex: "1",
+            left: "0px",
+            width: "100%",
           }}
         >
           <button className="negative-simple-button">
