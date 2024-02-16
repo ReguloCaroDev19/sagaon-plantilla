f2c9febc5ead7703828e88e63c96ad2f738a71a9
Francisco Alvarado <franciscolea@hotmail.com>
2024-02-15 19:41:04 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Plantilla } from './app/Plantilla';
+const router = createBrowserRouter([
+  {
+    path: '/:id',
+    element: <Plantilla />,
+  },
+]);
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
